afdf3158df3aff09899c51bd5c86f23aed4c0e12
diegozsr1 <diegozsr1@gmail.com>
2026-07-13 11:22:59 +0200

    Add SSL config for Aiven MySQL connection

--- src/config/db.js
+++ src/config/db.js
@@ -3,12 +3,18 @@ const mysql = require('mysql2/promise');
 
 require('dotenv').config();
 
+const fs = require('fs');
+
 const pool = mysql.createPool({
-    host: process.env.DB_HOST, // o la IP del servidor
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     port: process.env.DB_PORT,
-    database: process.env.DB_NAME_DB
+    database: process.env.DB_NAME_DB,
+    ssl: {
+        ca: fs.readFileSync(__dirname + '/ca.pem'),
+        rejectUnauthorized: true
+    }
 });
 
 module.exports = pool;
